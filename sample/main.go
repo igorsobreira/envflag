2620dc2d57404c11a3d96df2e5053c75d6246254
Igor Sobreira <igor@igorsobreira.com>
2016-08-26 09:08:56 -1000

    sample

---
+++ sample/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/igorsobreira/envflag"
+)
+
+func main() {
+
+	var url, secret string
+
+	flag.StringVar(&url, "url", "http://example.com", "API URL")
+	envflag.StringVar(&secret, "secret", "SECRET", "", "API Secret")
+
+	flag.Parse()
+	flag.VisitAll(envflag.Visit) // override flag values with env vars
+
+	log.Printf("url = %q", url)
+	log.Printf("secret = %q", secret)
+}
